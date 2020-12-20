b786cabb23c6332932597f57ae3f86c9c3d08b52
Antoine Godeau <antoinegodeau@pc192.home>
2020-12-20 14:40:26 +0100

    Test sonar

--- src/main/java/fr/orantoine/firsttest/FirstTestApplication.java
+++ src/main/java/fr/orantoine/firsttest/FirstTestApplication.java
@@ -1,7 +1,10 @@
 package fr.orantoine.firsttest;
 
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FirstTestApplication {
@@ -10,4 +13,10 @@ public class FirstTestApplication {
         SpringApplication.run(FirstTestApplication.class, args);
     }
 
+
+
+    @Bean
+    public TimedAspect timedAspect(MeterRegistry registry) {
+        return new TimedAspect(registry);
+    }
 }
--- src/main/java/fr/orantoine/firsttest/services/TestService.java
+++ src/main/java/fr/orantoine/firsttest/services/TestService.java
@@ -1,12 +1,15 @@
 package fr.orantoine.firsttest.services;
 
 
+import io.micrometer.core.annotation.Timed;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TestService {
 
 
+
+    @Timed(description = "Time spent return input")
     public String returnInput(String input){
         return input;
     }
